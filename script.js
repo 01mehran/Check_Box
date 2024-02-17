b56122a988a7d2212945f80f7a6616543e2b83e6
01mehran <132054519+01mehran@users.noreply.github.com>
2024-02-17 20:35:12 +0330

    created random numbers

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const secondN = document.querySelector("#second-n");
 const resultInput = document.querySelector("#result");
 const refreshBtn = document.querySelector("#refresh-btn");
 const checkBtn = document.querySelector("#check-btn");
+
+function randomNUmber() {
+  let min = 1;
+  let max = 100;
+
+  let random1 = Math.floor(Math.random() * (max - min)) + min;
+  let letter = random1 < 10 ? "0" + random1 : random1;
+  firstN.innerHTML = letter;
+
+  let random2 = Math.floor(Math.random() * (max - min)) + min;
+  let letter2 = random2 < 10 ? "0" + random2 : random2;
+  secondN.innerHTML = letter2;
+
+  const sum = random1 + random2;
+
+  
+}
+randomNUmber();
+
