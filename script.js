75e4afbe282f798101c45787f0101cd94d6eb638
01mehran <132054519+01mehran@users.noreply.github.com>
2024-02-17 20:33:18 +0330

    get the El that I need

---
+++ script.js
@@ -0,0 +1,5 @@
+const firstN = document.querySelector("#first-n");
+const secondN = document.querySelector("#second-n");
+const resultInput = document.querySelector("#result");
+const refreshBtn = document.querySelector("#refresh-btn");
+const checkBtn = document.querySelector("#check-btn");
